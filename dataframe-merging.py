38ce58c47214f2897fe9050bbf5af29bc9bff500
ZNevzz <dsouza.nevil45@gmail.com>
2018-03-25 15:32:10 +0530

    initial commit

---
+++ dataframe-merging.py
@@ -0,0 +1,48 @@
+'''dataframe operations in Python'''
+
+import pandas as pd
+import numpy as np
+
+'''break major df into n minor dfs and join using index '''
+total_values = 3
+a = ['A'+str(i) for i in range(total_values)]
+b = ['B'+str(i) for i in range(total_values)]
+c = ['C'+str(i) for i in range(total_values)]
+dataset = list(zip(a,b))
+df1 = pd.DataFrame(data = dataset,
+	columns = ['A','B'], index = a
+	)
+dataset = list(zip(a,c))
+df2 = pd.DataFrame(data = dataset,
+	columns = ['A','C'], index = a
+	)
+print(df1)
+print('-'*20)
+print(df2)
+print('-'*20)
+
+not_so_good = pd.concat([df1, df2], axis=0)
+'''not_so_good dataframe created'''
+print(not_so_good)
+print('-'*20)
+'''divide not_so_good into n minors and concat them column wise'''
+df3 = not_so_good[['A','B']].dropna()
+df3.set_index('A')
+df4 = not_so_good[['A','C']].dropna()
+df4.set_index('A')
+print(df3)
+print('-'*20)
+print(df4)
+print('-'*20)
+
+# much_better = pd.concat([df3, df4], axis = 1)
+# much_better = pd.concat([df3, df4], axis = 1, join = 'inner')
+much_better = pd.concat([df3, df4], axis =1 , join_axes = [df3.index])
+print(much_better)
+print('-'*20)
+
+''' 
+# IMPROVEMENTS
+1. In much_better, column 'A' is getting repeated. Why?
+2. Can this entire logic be written in R?
+'''

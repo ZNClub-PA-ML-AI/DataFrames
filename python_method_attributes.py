d3788499d29c6fefef5bcbabbd0af4d5cf387ef1
ZNevzz <dsouza.nevil45@gmail.com>
2018-07-07 20:11:15 +0530

    updates

---
+++ python_method_attributes.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun  9 20:09:39 2018
+
+@author: Augus
+"""
+def math(a=1, b=1):
+    return a+b
+
+math.__annotations__
+math.__call__
+math.__class__
+math.__closure__
+math.__code__
+math.__defaults__
+math.__delattr__
+math.__dict__
+math._dir__
+math.__doc__
+math.__eq__
+math.__format__
+math.__ge__
+math.__get__
+math.__getattribute__
+math.__globals__
+math.__gt__
+math.__hash__
+math.__init__
+math.__kwdefaults__
+math.__le__
+math.__lt__
+math.__module__
+math.__name__
+math.__ne__
+math.__new__
+math.__qualname__
+math.__reduce__
+math.__reduce_ex__
+math.__repr__
+math.__setattr__
+math.__sizeof__
+math.__str__
+math.__subclasshook__

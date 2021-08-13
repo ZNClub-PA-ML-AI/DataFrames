d8b73a59a92a7c1254699325b7ee3033911cf37a
Nevil Dsouza <dsouza.nevil45@gmail.com>
2021-08-13 08:36:38 +0530

    csv,clipboard added

--- python/dataframe-exporting.py
+++ python/dataframe-exporting.py
@@ -3,8 +3,27 @@
 import pandas as pd
 import numpy as np
 
+kwargs = {
+  'index': False,
+}
+
+df = pd.DataFrame(np.random.randn(5, 2), columns=list('AB'))
+
+'''To CLIPBOARD'''
+dfclip = df.copy()
+dfclip.to_clipboard()
+
+'''to CSV'''
+from csv import QUOTE_NONE, QUOTE_ALL, QUOTE_NONNUMERIC, QUOTE_MINIMAL # QUOTE_MINIMAL is default
+csv_kwargs = dict(quoting=QUOTE_ALL,)
+csv_kwargs.update(kwargs)
+
+dfcsv = df.copy()
+csv_in_string = dfcsv.to_csv(None, csv_kwargs)
+
+
 '''To JSON'''
-dfj = pd.DataFrame(np.random.randn(5, 2), columns=list('AB'))
+dfj = df.copy()
 
 '''
 orient: format of JSON string
@@ -30,3 +49,7 @@ json = dfj.to_json(orient='values')
 print(json, end='\n'+'-'*10+'\n')
 
 #date handling
+
+
+'''to EXCEL'''
+
